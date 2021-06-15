66248bf730524ddd272b7cad18500c48b1da64e1
lyonssp <lyonssp777@gmail.com>
2021-06-15 09:27:02 -0500

    fix build file and resource test

--- test/clozel/resource_test.clj
+++ test/clozel/resource_test.clj
@@ -1,9 +1,10 @@
 (ns resource.test
   (:require
     [clojure.test :refer [deftest testing is]]
-    [clojure.edn :as edn]))
+    [clojure.edn :as edn]
+    [clojure.java.io :as io]))
 
 (deftest test-read-from-file
   (testing "read message from file"
-    (let [data (:message (edn/read-string (slurp "data.edn")))]
+    (let [data (:message (edn/read-string (slurp (io/resource "data.edn"))))]
       (is (= data "Hello, World!")))))
